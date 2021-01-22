46c0b8dea5ed313aa8af2d8ddf97bb3699daabbc
Jacob Archambault <jacobarchambault@gmail.com>
2021-01-21 23:10:46 -0500

    Add MyFile class with allText method returning text from file

---
+++ src/com/jacobarchambault/fileencryption/MyFile.java
@@ -0,0 +1,16 @@
+package com.jacobarchambault.fileencryption;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+class MyFile {
+	String path;
+	MyFile(String filePath) {
+		path = filePath;
+	}
+	String allText() throws IOException{
+	    return new String(Files.readAllBytes(Paths.get(path))); 
+	}
+
+}
