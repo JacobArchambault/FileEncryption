cb56acad99a8acf1306ab620c6ed20873772fecb
Jacob Archambault <jacobarchambault@gmail.com>
2021-01-22 09:50:31 -0500

    Get text for Cipher from File

--- src/com/jacobarchambault/fileencryption/Program.java
+++ src/com/jacobarchambault/fileencryption/Program.java
@@ -1,10 +1,12 @@
 package com.jacobarchambault.fileencryption;
 
+import java.io.IOException;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		System.out.println(new CaesarCipher().cipher("1234567890", 3));
+		System.out.println(new CaesarCipher().cipher(new MyFile("MyLetters.txt").allText(), 3));
 	}
 
 }
