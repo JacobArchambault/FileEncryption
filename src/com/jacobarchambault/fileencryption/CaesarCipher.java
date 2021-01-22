eda4fe7a4ac1f5028c3c681d483c3acc145acc0a
Jacob Archambault <jacobarchambault@gmail.com>
2021-01-22 10:27:57 -0500

    Make getUserInput method private and clean up

--- src/com/jacobarchambault/fileencryption/CaesarCipher.java
+++ src/com/jacobarchambault/fileencryption/CaesarCipher.java
@@ -4,6 +4,23 @@ import java.util.Scanner;
 
 public class CaesarCipher {
 
+	private static int getUserInput(){
+		System.out.print("Enter an integer for your Caesar Cipher and press enter: ");
+		Scanner scanner = new Scanner(System.in);
+		int cipher = scanner.nextInt();
+		scanner.close();
+		return cipher;		
+	}
+
+	private char applyLowerCaseCipher(int offset,
+			char character) {
+		return (char) ('a' + ((character - 'a' + offset) % 26));
+	}
+
+	private char applyUpperCaseCipher(int offset,
+			char character) {
+		return (char) ('A' + ((character - 'A' + offset) % 26));
+	};
 	String cipher(String message) {
 		int offset = getUserInput();
 		System.out.println("Encrypting file...");
@@ -18,22 +35,7 @@ public class CaesarCipher {
 		}
 		return builder.toString();
 	}
-
-	private char applyLowerCaseCipher(int offset,
-			char character) {
-		return (char) ('a' + ((character - 'a' + offset) % 26));
-	}
-
-	private char applyUpperCaseCipher(int offset,
-			char character) {
-		return (char) ('A' + ((character - 'A' + offset) % 26));
-	};
-	static int getUserInput(){
-		System.out.print("Enter an integer for your Caesar Cipher and press enter: ");
-		Scanner scanner = new Scanner(System.in);
-		int cipher = scanner.nextInt();
-		scanner.close();
-		return cipher;		
-	}
+	
+	
 
 }
--- src/com/jacobarchambault/fileencryption/Program.java
+++ src/com/jacobarchambault/fileencryption/Program.java
@@ -1,7 +1,6 @@
 package com.jacobarchambault.fileencryption;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
 
