660df89cb2b3a04b20ca2ff659f1d936ca4e7df1
Jacob Archambault <jacobarchambault@gmail.com>
2021-01-22 11:44:08 -0500

    Add while loop for validating user input to key method

--- src/com/jacobarchambault/fileencryption/CaesarCipher.java
+++ src/com/jacobarchambault/fileencryption/CaesarCipher.java
@@ -8,9 +8,23 @@ public class CaesarCipher {
 		System.out.print("Enter an integer for your Caesar Cipher and press enter: ");
 		Scanner scanner = new Scanner(
 				System.in);
-		int cipher = Integer.parseInt(scanner.nextLine());
+		String input = scanner.nextLine();
+		while (!isNumber(input)) {
+			System.out.println("Invalid input. Please enter a number");
+			input = scanner.nextLine();
+		}
+		int cipher = Integer.parseInt(input);
 		scanner.close();
 		return cipher;
+	}
+
+	private static boolean isNumber(String input) {
+		try {
+			Integer.parseInt(input);
+			return true;
+		} catch (NumberFormatException ex) {
+			return false;
+		}
 	}
 
 	private char applyLowerCaseCipher(int offset,
