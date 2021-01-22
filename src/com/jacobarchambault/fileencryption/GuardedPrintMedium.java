be8cd0e31acb22d7d2c80037ee1adf78619c4b2b
Jacob Archambault <jacobarchambault@gmail.com>
2021-01-22 12:49:01 -0500

    Add guardedPrintMedium class implementing PrintMedium

---
+++ src/com/jacobarchambault/fileencryption/GuardedPrintMedium.java
@@ -0,0 +1,22 @@
+package com.jacobarchambault.fileencryption;
+
+import java.io.IOException;
+
+public class GuardedPrintMedium implements PrintMedium {
+
+	PrintMedium medium;
+
+	GuardedPrintMedium(PrintMedium origin){
+		medium = origin;
+	}
+	@Override
+	public String allText()  {
+			try {
+				return medium.allText();
+			} catch (IOException e) {
+				System.out.println("There was a problem reading the text file: using default text instead");
+			}
+			return "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	}
+	
+}
